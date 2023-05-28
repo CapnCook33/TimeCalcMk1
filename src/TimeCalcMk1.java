8af152f1ca74ff23f4e86795c2d3a4c05b9a05fd
Stephen Cook <121173622+CapnCook33@users.noreply.github.com>
2023-05-28 03:58:10 -0400

    23/05/28, 3:55am - Update 1 - First attempt at committing and pushing to Github using the features inside of IntelliJ.

---
+++ src/TimeCalcMk1.java
@@ -0,0 +1,11 @@
+/* First attempt at a basic time calculator. The user can enter the current date, or choose desired dates
+ * to calculate between, and the program will return the number of days between two dates.
+ * --Additional features to be added later.
+ * --This may later be added to the PlantPlanner/Personal Gardener app/program I've had in mind.
+ */
+
+public class TimeCalcMk1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
